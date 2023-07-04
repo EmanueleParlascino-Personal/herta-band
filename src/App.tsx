a86486e03c53595372d9ab391eb1dd8670fab954
Emanuele Parlascino <emapage95@gmail.com>
2023-07-04 15:25:33 +0200

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <nav>
+        <div className='nav-container'>
+              <ul>
+                <li><a href="#one">Shop</a></li>
+                <li><a href="#two">Shows</a></li>
+                <li><a href="#three">Gallery</a></li>
+                <li><a href="#four">News</a></li>
+                <li><a href="#five">Contact</a></li>
+              </ul>
+        </div>
+    </nav>
+      <section className="banner-section">
+        <h2>Banner</h2>
+        <div className='shop-item'>this is the banner space</div>
+      </section>
+      <section id="one" className="shop-section">
+        <h2>Shop</h2>
+        <div className='shop-item'>this is a shop item</div>
+      </section>
+      <section id="two" className="shows-section">
+        <h2>Shows</h2>
+      </section>
+      <section id="three" className="gallery-section">
+        <h2>Gallery</h2>
+      </section>
+      <section id="four" className="news-section">
+        <h2>News</h2>
+      </section>
+      <section id="five" className="contact-section">
+        <h2>Contact</h2>
+      </section>
     </>
   )
 }
