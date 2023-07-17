6605c9c4d6d8f613f2815e63eab98a0817ebeb9b
Emanuele Parlascino <emapage95@gmail.com>
2023-07-17 11:31:21 +0200

    product page style

--- pages/Components/Shop.tsx
+++ pages/Components/Shop.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from "../Styles/shop.module.css"
 import { urlFor } from '../../lib/client'
+import Link from 'next/link'
+
 
 const Shop = ({products}) => {
     const clothing = products?.filter(p => p.category === "Clothing")
@@ -17,23 +19,29 @@ const Shop = ({products}) => {
             </div>
             <div className={styles.wrapper}>
                 {clothing?.map(p => 
-                <div className={styles.clippedBorder}>
-                    <img src ={urlFor(p.image[0])} className={styles.image} />
-                </div>)}
+                <Link href = {`/product/${p.slug.current}`}>
+                    <div className={styles.clippedBorder}>
+                        <img src ={urlFor(p.image[0])} className={styles.image} />
+                    </div>
+                </Link>)}
             </div>
             <h2 className='title'>Accessories</h2>
             <div className={styles.wrapper}>
                 {accessories?.map(p => 
-                <div className={styles.clippedBorder}>
-                    <img src ={urlFor(p.image[0])} className={styles.image} />
-                </div>)}
+                <Link href = {`/product/${p.slug.current}`}>
+                    <div className={styles.clippedBorder}>
+                        <img src ={urlFor(p.image[0])} className={styles.image} />
+                    </div>
+                </Link>)}
             </div>
             <h2 className='title'>Tabs</h2>
             <div className={styles.wrapper}>
                 {tabs?.map(p => 
-                <div className={styles.clippedBorder}>
-                    <img src ={urlFor(p.image[0])} className={styles.image} />
-                </div>)}
+                <Link href = {`/product/${p.slug.current}`}>
+                    <div className={styles.clippedBorder}>
+                        <img src ={urlFor(p.image[0])} className={styles.image} />
+                    </div>
+                </Link>)}
             </div>
         </div>
     )
---
+++ pages/product/[slug].js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { useState } from 'react'
+import {client , urlFor} from '../../lib/client'
+import styles from '../Styles/product.module.css'
+
+const ProductDetails = ({products, product}) => {
+    const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
+    return (
+        <div className={styles.container}>
+            <div>
+            <img src={urlFor(image[index])} className={styles.productImage} />
+            <div className={styles.smallImagesContainer}>
+                {image?.map((item, i) => (
+                <img 
+                    key={i}
+                    src={urlFor(item)}
+                    className={i === index ? `${styles.smallImage} ${styles.selectedImage}` : styles.smallImage}
+                    onMouseEnter={() => setIndex(i)}
+                />
+                ))}
+            </div>
+            </div>
+            <div className={styles.text}>
+                <h1 className={styles.name}>{name}</h1>
+                <p className={styles.price}>{price} Euro</p>
+                <h4 className={styles.detailsTitle}>Details: </h4>
+                <p className={styles.detailsBody}>{details}</p>    
+                <div>
+                    <button className={styles.addToCart}>Buy now</button>
+                    <button className={styles.addToCart}>Add to cart</button>
+                </div>   
+            </div>
+            
+        </div>
+    )
+}
+
+
+export const getStaticPaths = async () =>{
+    const query = `*[_type == "product"]{
+        slug{
+            current
+        }
+    }`
+    const products = await client.fetch(query);
+
+    const paths = products.map(p => ({
+        params:{
+            slug: p.slug.current
+        }
+    }));
+
+    return{
+        paths,
+        fallback: 'blocking'
+    }
+}
+export const getStaticProps = async ({params: {slug}}) => {
+    const productQuery = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const product = await client.fetch(productQuery);
+
+    const query = `*[_type == "product" && category.current == '${product.category}']`;  //testing out getting products in the same category
+    const products = await client.fetch(query);
+
+    return {
+        props:{products, product}
+    }
+
+}
+
+export default ProductDetails
